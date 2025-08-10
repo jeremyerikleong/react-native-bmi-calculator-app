6362413e719a3be2eb4dded8d00a1aee1b45303f
Jeremy <jeremythegreat95@gmail.com>
2025-08-11 01:03:40 +0800

    built counter component

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,14 +1,19 @@
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { Pressable, SafeAreaView, StyleSheet, Text } from 'react-native';
+import CounterContainer from './CounterContainer';
 import GenderSelection from './GenderSelection';
 import HeightSlider from './HeightSlider';
 
-import { SIZES } from '@/constants/theme';
+import { COLORS, SIZES } from '@/constants/theme';
 
 export default function Calculator() {
     return (
         <SafeAreaView style={styles.container}>
             <GenderSelection />
             <HeightSlider />
+            <CounterContainer />
+            <Pressable style={styles.btnCalculate}>
+                <Text style={styles.btnCalculateText}>Calculate</Text>
+            </Pressable>
         </SafeAreaView>
     )
 }
@@ -16,5 +21,16 @@ export default function Calculator() {
 const styles = StyleSheet.create({
     container: {
         gap: SIZES.medium
+    },
+    btnCalculate: {
+        backgroundColor: COLORS.btnPrimary,
+        borderRadius: SIZES.small / 2,
+        paddingVertical: SIZES.medium
+    },
+    btnCalculateText: {
+        color: COLORS.defaultText,
+        fontSize: SIZES.large,
+        fontWeight: 500,
+        textAlign: 'center'
     }
 })
---
+++ components/Counter.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import { COLORS, SIZES } from '@/constants/theme';
+
+export default function Counter({ title }: { title: string }) {
+    const [amount, setAmount] = useState(0);
+
+    function handleAmountIncrement() {
+        setAmount(prev => prev + 1);
+    }
+
+    function handleAmountDecrement() {
+        if (amount >= 1) {
+            setAmount(prev => prev - 1);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            <View style={styles.amountDisplayContainer}>
+                <Text style={styles.amountText}>{amount}</Text>
+                {title === 'weight' && <Text style={styles.unitText}>kg</Text>}
+            </View>
+            <View style={styles.btnCounterContainer}>
+                <TouchableOpacity
+                    style={styles.btnCounter}
+                    onPress={handleAmountDecrement}>
+                    <Text style={styles.btnCounterText}>-</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btnCounter}
+                    onPress={handleAmountIncrement}>
+                    <Text style={styles.btnCounterText}>+</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1 / 2,
+        backgroundColor: COLORS.foreground,
+        borderRadius: SIZES.xSmall,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: SIZES.large,
+    },
+    sliderContainer: {
+        alignItems: 'center'
+    },
+    title: {
+        fontSize: SIZES.large,
+        fontWeight: 500,
+        color: COLORS.defaultText,
+        textTransform: 'capitalize'
+    },
+    amountDisplayContainer: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        gap: SIZES.xSmall,
+        marginVertical: SIZES.medium
+    },
+    amountText: {
+        fontSize: SIZES.medium * 2,
+        color: COLORS.defaultText
+    },
+    unitText: {
+        fontSize: SIZES.medium,
+        color: COLORS.defaultText,
+        marginBottom: 4
+    },
+    btnCounterContainer: {
+        flexDirection: 'row',
+        gap: SIZES.xSmall
+    },
+    btnCounter: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: SIZES.small * 3,
+        height: SIZES.small * 3,
+        borderRadius: SIZES.medium,
+        backgroundColor: COLORS.background
+    },
+    btnCounterText: {
+        color: COLORS.defaultText,
+        fontSize: SIZES.medium,
+        fontWeight: 500
+    }
+})
